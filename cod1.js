3404789a5c13771ea58cc44e03ad507c7362813c
gchiantore <102333574+gchiantore@users.noreply.github.com>
2024-08-28 17:29:00 -0300

    LUZU TV

---
+++ cod1.js
@@ -0,0 +1,6 @@
+const pi=3.14;
+console.log(pi*2);
+console.log('*****************************************************');
+console.log('* HOLA MUNDO ESTA ES MI PRIMERA PRUEBA BACKEND JUJU *');
+console.log('*****************************************************');
+
